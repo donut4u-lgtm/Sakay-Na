210094a3401b1b8277ac52d4694b452278c9c24f
donut4u-lgtm <donut4u@gmail.com>
2026-09-10 20:24:53 +0800

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.sakyna.app"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.sakyna.app"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
