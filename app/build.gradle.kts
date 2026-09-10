e3e92f32d030876f82b23932bacfcaa68f03500e
donut4u-lgtm <donut4u@gmail.com>
2026-09-10 22:17:36 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,4 @@
+
 plugins {
     id("com.android.application")
 }
@@ -12,5 +13,16 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
