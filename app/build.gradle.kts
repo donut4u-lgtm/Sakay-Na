e04f418d633bae0c6c938c46da72a1f42bcc3008
donut4u-lgtm <donut4u@gmail.com>
2026-09-12 23:45:14 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,4 +34,6 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:34.18.0"))
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
+
+    implementation("org.osmdroid:osmdroid-android:6.1.20")
 }
